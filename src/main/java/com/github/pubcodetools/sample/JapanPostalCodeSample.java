7f4d70f5353d27ca5030f6b830099be7573f31fa
PubCodeTools <pubcodetools@gmail.com>
2021-04-23 19:28:04 +0900

    first commit

---
+++ src/main/java/com/github/pubcodetools/sample/JapanPostalCodeSample.java
@@ -0,0 +1,32 @@
+package com.github.pubcodetools.sample;
+
+import com.github.pubcodetools.japanPostalCode.JapanPostalCodes;
+import com.github.pubcodetools.japanPostalCode.JapanPostalCode;
+
+import java.util.List;
+
+public class JapanPostalCodeSample {
+    public static void main(String ...args) {
+        JapanPostalCodes jpcs = new JapanPostalCodes();
+
+        {
+            JapanPostalCode jpc = jpcs.get("0600000");
+            System.out.println(jpc.getPostalCode());
+            System.out.println(jpc.getPrefecture());
+            System.out.println(jpc.getCity());
+            System.out.println(jpc.getStreet());
+        }
+
+        {
+            List<JapanPostalCode> jpcList = jpcs.getAll();
+            for (JapanPostalCode jpc : jpcList) {
+                System.out.println(jpc.toString());
+            }
+        }
+
+        {
+            String version = jpcs.getVersion();
+            System.out.println(version);
+        }
+    }
+}
