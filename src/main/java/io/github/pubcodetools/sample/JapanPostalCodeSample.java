54670c8b84a916af3d28228adb66d94b77ba6eb0
PubCodeTools <pubcodetools@gmail.com>
2021-04-27 15:22:57 +0900

    Add codestyle

--- src/main/java/io/github/pubcodetools/sample/JapanPostalCodeSample.java
+++ src/main/java/io/github/pubcodetools/sample/JapanPostalCodeSample.java
@@ -1,32 +1,35 @@
 package io.github.pubcodetools.sample;
 
-import io.github.pubcodetools.japanPostalCode.JapanPostalCodes;
-import io.github.pubcodetools.japanPostalCode.JapanPostalCode;
-
+import io.github.pubcodetools.japanpostalcode.JapanPostalCode;
+import io.github.pubcodetools.japanpostalcode.JapanPostalCodes;
 import java.util.List;
 
 public class JapanPostalCodeSample {
-    public static void main(String ...args) {
-        JapanPostalCodes jpcs = new JapanPostalCodes();
+  /**
+   * Sample application main.
+   * @param args List of String
+   */
+  public static void main(String... args) {
+    JapanPostalCodes jpcs = new JapanPostalCodes();
 
-        {
-            JapanPostalCode jpc = jpcs.get("0600000");
-            System.out.println(jpc.getPostalCode());
-            System.out.println(jpc.getPrefecture());
-            System.out.println(jpc.getCity());
-            System.out.println(jpc.getStreet());
-        }
-
-        {
-            List<JapanPostalCode> jpcList = jpcs.getAll();
-            for (JapanPostalCode jpc : jpcList) {
-                System.out.println(jpc.toString());
-            }
-        }
-
-        {
-            String version = jpcs.getVersion();
-            System.out.println(version);
-        }
+    {
+      JapanPostalCode jpc = jpcs.get("0600000");
+      System.out.println(jpc.getPostalCode());
+      System.out.println(jpc.getPrefecture());
+      System.out.println(jpc.getCity());
+      System.out.println(jpc.getStreet());
     }
+
+    {
+      List<JapanPostalCode> jpcList = jpcs.getAll();
+      for (JapanPostalCode jpc : jpcList) {
+        System.out.println(jpc.toString());
+      }
+    }
+
+    {
+      String version = jpcs.getVersion();
+      System.out.println(version);
+    }
+  }
 }
